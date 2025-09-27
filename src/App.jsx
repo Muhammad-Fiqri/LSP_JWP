f182c884d6deb6b866e7cbd958407e2a6c482572
Muhammad Fiqri <muhfikri6565@gmail.com>
2025-09-27 11:40:36 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import TopNav from './TopNav'
+
+function App() {
+
+  return (
+    <>
+      <TopNav/>
+    </>
+  )
+}
+
+export default App
---
+++ src/TopNav.jsx
@@ -0,0 +1,19 @@
+
+export default function TopNav() {
+    return(
+        <div id="TopNav">
+            <button>
+                Home
+            </button>
+            <button>
+                Katalog
+            </button>
+            <button>
+                Order
+            </button>
+            <button>
+                Login
+            </button>
+        </div>
+    )
+}
